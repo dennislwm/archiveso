cf8fb1526d4fd85557bd91abba536d9d7ceac382
Dennis Lee <dennislwm@yahoo.com.au>
2022-02-10 15:07:12 +0800

    Complete Python Flask server authentication.
    This is part 2 of 5 parts of the build cycle.
    
    We are using HTTP basic authentication for our Python Flask server.
    This is to prevent any unauthorized persons to access our API endpoints,
    which are available via a public domain.
    
    There are many methods of storing and retrieving secrets. However, in
    this build we will only be covering the use of environment variables.
    
    We need to ensure that the secrets are set in the environment variables on:
    
    - a Python flask server when running locally
    - a Docker container server when running locally
    - a Docker container server when running in our CI pipeline
    - a Docker container server when running in our production instance
    
    Next part is to build a Lowdefy page to allow user interaction.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,30 @@
+import os
+
 from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def create_app():
     app = Flask(__name__)
+    auth = HTTPBasicAuth()
+
+    API_USERNAME = os.getenv('LOWDEFY_SECRET_API_USERNAME', '')
+    API_PASSWORD = os.getenv('LOWDEFY_SECRET_API_PASSWORD', '')
+
+    users = {
+        API_USERNAME: generate_password_hash(API_PASSWORD),
+    }
+
+    @auth.verify_password
+    def verify_password(username, password):
+        if username in users:
+            return check_password_hash(users.get(username), password)
+        return False
 
     @app.route("/")
+    @auth.login_required
     def root():
-        return "App-version: 0.1.0"
+        return "App-version: 0.2.0"
 
     return app
 
